3920482261f06e72bf45effa8974b76812f65a60
kim kirui <115598773+maninthehood@users.noreply.github.com>
2023-07-11 14:41:57 +0000

    Create 4 files

---
+++ apis/weather_api.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+
+const req = http.request('https://catfact.ninja/fact', res => {
+	const data = [];
+
+	res.on('data', _ => data.push(_))
+	res.on('end', () => console.log(data.join()))
+});
+
+req.end();
+
+// server.listen(PORT, () => {
+//   console.log(`Server running at PORT:${PORT}/`);
+// });
